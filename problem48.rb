52a6e7eca77ae3babb68f83a4aba105e1293c923
Brian Summerfield <brianjsummerfield@gmail.com>
2019-04-27 20:26:05 -0500

    solve problem48

--- problem48.rb
+++ problem48.rb
@@ -1,3 +1,7 @@
+# The series, 11 + 22 + 33 + ... + 1010 = 10405071317.
+
+# Find the last ten digits of the series, 11 + 22 + 33 + ... + 10001000.
+
 def problem48
   answer = 0
   1000.times do |i|
