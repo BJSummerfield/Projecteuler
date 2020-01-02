4b99a29a8b0ccac4eb5ccd9c3ccc5e7270285274
Brian Summerfield <brianjsummerfield@gmail.com>
2020-01-02 17:03:58 -0600

    problem 21 solved

---
+++ problem21.rb
@@ -0,0 +1,30 @@
+def runner(number)
+  amicable_nums = []
+  number.times do |i|
+    first = proper_divisors(i+1)
+    second = proper_divisors(first)
+    if second == i+1 && first != second
+      unless amicable_nums.include?(second)
+        amicable_nums << second
+      end
+    end
+  end
+  puts amicable_nums.inject(:+)
+end
+
+
+
+def proper_divisors(num)
+  arr = []
+  if num != nil
+    (num - 1).times do |i|
+      div = i + 1
+      if num % div == 0
+        arr << div
+      end
+    end
+    return arr.inject(:+)
+  end
+end
+
+runner(10000)
